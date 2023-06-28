8e13ddf72dad00405e7112cf687cec934f6095be
jobodd <38688162+jobodd@users.noreply.github.com>
2023-06-28 21:20:43 +0100

    some stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+
+  spells: any[] = [
+    {
+      'name': 'a spell name!',
+      'v': true,
+      's': true,
+      'm': true,
+      'level': "C",
+      'castTime': 'Action',
+      'isPrepared': false
+    },
+    {
+      'name': 'A powerful spell!',
+      'v': true,
+      's': false,
+      'm': false,
+      'level': "1",
+      'castTime': 'Action',
+      'isPrepared': false
+    }
+  ]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+
+
+}
